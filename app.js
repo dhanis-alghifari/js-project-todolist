2109450a166a6c0bf87fc181e213d2dd7f202b7c
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-08-04 23:21:31 +0700

    collecting ui elements

---
+++ app.js
@@ -0,0 +1,6 @@
+// *Collect All UI Elements
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const filterInput = document.querySelector("#filter-input");
+const todoList = document.querySelector("#todo-list");
+const clearButton = document.querySelector("#clear-todos");
